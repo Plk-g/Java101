88d0ffe382a21f3e87d24013d6c08648a9e7c8b8
Palak Gupta <palakg008@gmail.com>
2025-11-13 11:49:25 -0500

    Add Assignment 1 - basic Java exercises

---
+++ Assignment1/PartIII/BlackbodyRadiation.java
@@ -0,0 +1,17 @@
+public class BlackbodyRadiation {
+
+	public static double calculateEnergy () {
+		
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		double energy = calculateEnergy();
+		System.out.println("I for the Sun is ");
+		System.out.println("I for the betelgeuse is ");
+		
+
+	}
+}
